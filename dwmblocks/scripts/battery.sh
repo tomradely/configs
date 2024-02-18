007dbb9620c2462b036cfcf79ab50c5725122108
tomradely <mcnabb14@gmail.com>
2024-02-18 17:20:45 +0000

    Changes to be committed: 	modified:   dwmblocks/scripts/battery.sh

--- dwmblocks/scripts/battery.sh
+++ dwmblocks/scripts/battery.sh
@@ -3,7 +3,18 @@ capacity=$(cat /sys/class/power_supply/BAT0/capacity 2>&1)
 case "$(cat "/sys/class/power_supply/BAT0/status" 2>&1)" in
 	"Full") status="󰚥" ;;
 	"Not charging") status="󰚥" ;;
-	"Charging") status="󰂅" ;;
-	"Discharging") status="󱟞" ;;
+	"Charging") status="󰂄" ;;
+	"Discharging") set -- $1 && case 1 in
+		$((capacity >=90)) ) status="󰂂" ;;
+		$((capacity >=80)) ) status="󰂁" ;;
+		$((capacity >=70)) ) status="󰂀" ;;
+		$((capacity >=60)) ) status="󰁿" ;;
+		$((capacity >=50)) ) status="󰁾" ;;
+		$((capacity >=40)) ) status="󰁽" ;;
+		$((capacity >=30)) ) status="󰁼" ;;
+		$((capacity >=20)) ) status="󰁻" ;;
+		$((capacity >=10)) ) status="󰁺 !" ;;
+		$((capacity >=5)) ) status="󰂎 !!" ;;
+	 esac;;
 esac
 printf " %s %s%% " "$status" "$capacity"
