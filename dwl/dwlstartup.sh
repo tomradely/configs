e125bf426f6eb493d7314cbb494d9252d93fb40d
tomradely <mcnabb14@gmail.com>
2026-01-28 13:38:09 +0800

    last commit to dwl for now

--- dwl/dwlstartup.sh
+++ dwl/dwlstartup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-exec ~/.config/dwl/swayidle.sh &
-wbg /home/tom/Pictures/gardenofwords1.jpg &
+# System
 exec systemd --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
 exec dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway &
 systemctl --user restart pipewire.service &
+exec ~/.config/dwl/swayidle.sh &
+kanshi &
+lxqt-policykit-agent &
+wl-paste --watch cliphist store &
+# Interface and Appearance
+waybar &
+dunst &
+wbg /home/tom/Pictures/gardenofwords1.jpg &
+# Autostarted applications
+/opt/Mullvad\ VPN/mullvad-gui &
+foot &
+floorp &
--- dwl/volume.sh
+++ dwl/volume.sh
@@ -4,65 +4,60 @@
 # $./volume.sh down
 # $./volume.sh mute
 # 
-# function get_volume currently does not get volume for all devices and must be brought up to date for pipewire
 
 function get_volume {
-    amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
+    pactl get-sink-volume \@DEFAULT_SINK@ | head -n 1 | cut -d '/' -f 2 | cut -d '%' -f 1 
 }
 
-function is_mute {
-    amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
+function muted {
+	pactl get-sink-mute \@DEFAULT_SINK@ | grep -oE '[^ ]+$' | grep yes > /dev/null
 }
-function send_notification {
+
+# If using images for notifications, consider using one for up and one for down.
+function send_notification_up {
     volume=`get_volume`
     bar=$(seq -s "─" $(($volume / 2)) | sed 's/[0-9]//g')
-    dunstify -t 8000 -r 2593 -u normal -h int:value:"$volume" "Volume: ${volume}%"
+    dunstify -i ~/.icons/Papirus-Dark/48x48/status/notification-audio-volume-high.svg -t 8000 -r 2593 -u normal -h int:value:"$volume" "Volume: ${volume}%"
+}
+function send_notification_down {
+    volume=`get_volume`
+    bar=$(seq -s "─" $(($volume / 2)) | sed 's/[0-9]//g')
+    dunstify -i ~/.icons/Papirus-Dark/48x48/status/notification-audio-volume-medium.svg -t 8000 -r 2593 -u normal -h int:value:"$volume" "Volume: ${volume}%"
 }
 
-# Notifications with images are currently throwing errors, presumably due to an issue with Dunst
+# If not using images, one notification entry will suffice
 #function send_notification {
 #    volume=`get_volume`
 #    bar=$(seq -s "─" $(($volume / 2)) | sed 's/[0-9]//g')
-#    dunstify -i ~/.local/share/icons/GNUstep/status/48/audio-volume-medium.png -t 8000 -r 2593 -u normal -h int:value:"$volume" "Volume: ${volume}%"
+#    dunstify -t 8000 -r 2593 -u normal -h int:value:"$volume" "Volume: ${volume}%"
 #}
 
-function send_notification1 {
-    volume=`get_volume`
-    bar=$(seq -s "─" $(($volume / 2)) | sed 's/[0-9]//g')
-    dunstify -t 8000 -r 2593 -u normal -h int:value:"$volume" "Volume: ${volume}%"
-}
-
-#function send_notification1 {
-#    volume=`get_volume`
-#    bar=$(seq -s "─" $(($volume / 2)) | sed 's/[0-9]//g')
-#    dunstify -i ~/.local/share/icons/GNUstep/status/48/audio-volume-low.png -t 8000 -r 2593 -u normal -h int:value:"$volume" "Volume: ${volume}%"
-#}
 case $1 in
     up)
-	# Set the volume on (if it was muted)
-	amixer -D pulse set Master on > /dev/null
-	amixer -D pulse sset Master 2%+,2%+ > /dev/null
-	send_notification
+	if muted ; then
+		pactl set-sink-mute \@DEFAULT_SINK@ off
+		send_notification_up
+	else
+	pactl set-sink-volume \@DEFAULT_SINK@ +2%
+	send_notification_up
+	fi
 	;;
     down)
-	amixer -D pulse set Master on > /dev/null
-	amixer -D pulse sset Master 2%-,2%- > /dev/null
-	send_notification1
+	if muted ; then
+		pactl set-sink-mute \@DEFAULT_SINK@ off
+		send_notification_down
+	else
+	pactl set-sink-volume \@DEFAULT_SINK@ -2%
+	send_notification_down
+	fi
 	;;
     mute)
-    	# Toggle mute
-	amixer -D pulse set Master 1+ toggle > /dev/null
-	if is_mute ; then
-       dunstify -t 8000 -r 2593 -u normal "Volume: Mute"
-    else
-       send_notification
+	pactl set-sink-mute \@DEFAULT_SINK@ toggle
+	if muted ; then
+		dunstify -i ~/.icons/Papirus-Dark/48x48/status/notification-audio-volume-muted.svg -t 8000 -r 2593 -u normal "Volume: Mute"
+	else
+		send_notification_up
 	fi
-
-#	if is_mute ; then
-#       dunstify -i ~/.local/share/icons/GNUstep/status/48/audio-volume-muted.png -t 8000 -r 2593 -u normal "Volume: Mute"
-#    else
-#       send_notification
-#	fi
 	;;
 esac
 
